157970d28d75f78a4216e4cda23953a2b1f55e57
dimonbat <dimon.bat@rambler.ru>
2013-10-19 22:28:53 +0600

    изменен цикл

--- vcfcut.py
+++ vcfcut.py
@@ -1,33 +1,25 @@
 f=open('In.vcf','r',encoding='utf8')
 
-line1='BEGIN:VCARD\n\r'
 fn = 0
-i=1
-while i<2000:
-	i=i+1
-	print (u'i')
-	print(i)
-
+stop = 0
+while stop != 1:
 	line=f.readline()
 	if not line: break
-	if 'BEGIN:' in line:
+
+	#find begin of card
+	if 'BEGIN:VCARD' in line:
 		fn = fn + 1
-		print (u'fn')
-		print (fn)
 		filename=str(fn)+'.vcf'
-		print (filename)
+		print (u'create file: ',filename)
 		ff=open(filename,"w")
 		ff.write(line)
 		ENDFLAG=0
 		while ENDFLAG != 1:
 			line=f.readline()
-			if not line: break
+			if not line: 
+				stop = 1
+				break
 			ff.write(line)
+			# find end of card
 			if 'END:VCARD' in line:ENDFLAG=1
-
-	
-
-
-
-
 
